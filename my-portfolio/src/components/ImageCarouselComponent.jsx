07a992fcdad30a11c5daa02650d7a07926802eab
Adelina Vaitus <adelinavaitus@gmail.com>
2025-02-12 17:09:25 +0200

    Added coursera verification links on the photos

--- my-portfolio/src/components/ImageCarouselComponent.jsx
+++ my-portfolio/src/components/ImageCarouselComponent.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function ImageCarousel() {
     return (
         <Carousel className='carousel-container'>
             <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="/assets/coursera/java_course.jpeg"
-                    alt="First slide"
-                />
+                <Link to="https://www.coursera.org/account/accomplishments/specialization/D9CRZ6196WZH">
+                    <img
+                        className="d-block w-100"
+                        src="/assets/coursera/java_course.jpeg"
+                        alt="First slide"
+                    />
+                </Link>
             </Carousel.Item>
+
             <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="/assets/coursera/spring_course.jpeg"
-                    alt="Second slide"
-                />
+                <Link to="https://www.coursera.org/account/accomplishments/specialization/6XXLOAZPRI8U">
+                    <img
+                        className="d-block w-100"
+                        src="/assets/coursera/spring_course.jpeg"
+                        alt="Second slide"
+                    />
+                </Link>
             </Carousel.Item>
+
             <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="/assets/coursera/react_course.jpeg"
-                    alt="Third slide"
-                />
+                <Link to="https://www.coursera.org/account/accomplishments/verify/H35CPZ57F3V2">
+                    <img
+                        className="d-block w-100"
+                        src="/assets/coursera/react_course.jpeg"
+                        alt="Third slide"
+                    />
+                </Link>
             </Carousel.Item>
         </Carousel>
     );
