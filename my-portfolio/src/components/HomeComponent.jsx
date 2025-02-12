0aeef298c8f1c591f52d2323e037fe8cbc21c132
Adelina Vaitus <adelinavaitus@gmail.com>
2025-02-12 13:43:20 +0200

    Worked on Home Page

--- my-portfolio/src/components/HomeComponent.jsx
+++ my-portfolio/src/components/HomeComponent.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { Card, CardTitle, CardBody, Button, CardText, CardHeader, Row, Col } from "react-bootstrap";
+import { FaMapMarkerAlt, FaGithub, FaLinkedin  } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 function Home() {
-    return <h1>This is the home page</h1>;
+    return (
+        <div className="container">
+            <Row>
+                <Col>
+                    <img
+                        src="/assets/portfolio.png"
+                        alt="Adelina's portfolio"
+                        style={{ width: "100%", objectFit: "cover" }}
+                    />
+                    <h3>
+                        Software Engineer
+                    </h3>
+
+                    <div>
+                        <FaMapMarkerAlt style={{ fontSize: "20px" }} />
+                            Bucharest, Romania
+                    </div>
+
+                    <div>
+                        <Button variant="primary" style={{ marginRight: '10px' }}>Lets colaborate</Button>
+                        <Button variant="secondary">See my work</Button>
+                    </div>
+
+                    <div>
+                        <MdEmail />
+                        <FaGithub />
+                        <FaLinkedin  />
+                    </div>
+                </Col>
+
+                <Col>
+                    <img
+                        src="/assets/home-img.png"
+                        alt="Home page image"
+                        style={{ width: "100%", objectFit: "cover" }}
+                    />
+                </Col>
+            </Row>
+        </div>
+    );
 }
 
 export default Home;
