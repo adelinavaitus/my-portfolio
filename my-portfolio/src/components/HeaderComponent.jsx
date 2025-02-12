7cf08d2674737685c503bd7f3affbb147da6552f
Adelina Vaitus <adelinavaitus@gmail.com>
2025-02-12 10:49:21 +0200

    Added styling for Links

--- my-portfolio/src/components/HeaderComponent.jsx
+++ my-portfolio/src/components/HeaderComponent.jsx
@@ -9,27 +9,27 @@ function Header() {
                 <Nav className="ms-auto">
                     <Nav.Item className="nav-item">
                         <div className="nav-icon" style={{ backgroundImage: 'url(/assets/washi-tape-home.png)' }}>
-                            <Link to="/">Home</Link>
+                            <Link to="/" className="link-style">Home</Link>
                         </div>
                     </Nav.Item>
                     <Nav.Item className="nav-item">
                         <div className="nav-icon" style={{ backgroundImage: 'url(/assets/washi-tape-aboutme.png)' }}>
-                            <Link to="/about">About me</Link>
+                            <Link to="/about" className="link-style">About me</Link>
                         </div>
                     </Nav.Item>
                     <Nav.Item className="nav-item">
                         <div className="nav-icon" style={{ backgroundImage: 'url(/assets/washi-tape-stack.png)' }}>
-                            <Link to="/stack">My stack</Link>
+                            <Link to="/stack" className="link-style">My stack</Link>
                         </div>
                     </Nav.Item>
                     <Nav.Item className="nav-item">
                         <div className="nav-icon" style={{ backgroundImage: 'url(/assets/washi-tape-projects.png)' }}>
-                            <Link to="/projects">Projects</Link>
+                            <Link to="/projects" className="link-style">Projects</Link>
                         </div>
                     </Nav.Item>
                     <Nav.Item className="nav-item">
                         <div className="nav-icon" style={{ backgroundImage: 'url(/assets/washi-tape-contactme.png)' }}>
-                            <Link to="/contact">Contact</Link>
+                            <Link to="/contact" className="link-style">Contact</Link>
                         </div>
                     </Nav.Item>
                 </Nav>
