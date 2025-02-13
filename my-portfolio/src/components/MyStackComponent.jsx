42448eaf6f687c9f64717c29cdc6cb03b1634cd3
Adelina Vaitus <adelinavaitus@gmail.com>
2025-02-13 15:24:47 +0200

    Added styling for My Stack page. Deleted unused image.

--- my-portfolio/src/components/MyStackComponent.jsx
+++ my-portfolio/src/components/MyStackComponent.jsx
@@ -10,13 +10,10 @@ function Stack() {
     return (
         <div className="container">
             <h2>My Tech Stack</h2>
-            <Row>
-                <Col><span className="tech-category-text">Backend</span></Col>
-                <Col><span className="tech-category-text">Frontend</span></Col>
-                <Col><span className="tech-category-text">Other technologies</span></Col>
-            </Row>
             <Row className="row-container">
-                <Col md={4} sm={6} className="my-stack-col" >
+
+                <Col lg={4} md={6} sm={12}  className="my-stack-col" >
+                    <span className="tech-category-text">Backend</span>
                     <Card className="tech-card">
                         <Card.Img
                             variant="top"
@@ -29,13 +26,13 @@ function Stack() {
                                 <Row className="tech-row">
                                     <Col xs={6} sm={6} md={6} lg={6} xl={6} >
                                         <div className="tech-icons">
-                                            <FaJava className="icon fs-1" />
+                                            <FaJava className="icon" />
                                             <span className="icon-tooltip">Java</span>
                                         </div>
                                     </Col>
                                     <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                                         <div className="tech-icons">
-                                            <SiSpring className="icon fs-1" />
+                                            <SiSpring className="icon" />
                                             <span className="icon-tooltip">Spring</span>
                                         </div>
                                     </Col>
@@ -43,13 +40,13 @@ function Stack() {
                                 <Row className="tech-row">
                                     <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                                         <div className="tech-icons">
-                                            <SiApachemaven className="icon fs-1" />
+                                            <SiApachemaven className="icon" />
                                             <span className="icon-tooltip">Maven</span>
                                         </div>
                                     </Col>
                                     <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                                         <div className="tech-icons">
-                                            <SiMysql className="icon fs-1" />
+                                            <SiMysql className="icon" />
                                             <span className="icon-tooltip">MySQL</span>
                                         </div>
                                     </Col>
@@ -60,7 +57,8 @@ function Stack() {
                     </Card>
                 </Col>
 
-                <Col md={4} sm={6} className="my-stack-col">
+                <Col lg={4} md={6} sm={12} className="my-stack-col">
+                    <span className="tech-category-text">Frontend</span>
                     <Card className="tech-card">
                         <Card.Img
                             variant="top"
@@ -68,19 +66,18 @@ function Stack() {
                             alt="Notepad"
                             className="notepad-img"
                         />
-
                         <Card className="overlaying-card position-absolute">
                             <div className="tech-card-img-container">
                                 <Row className="tech-row">
                                     <Col xs={6} sm={6} md={6} lg={6} xl={6} >
                                         <div className="tech-icons">
-                                            <FaJs className="icon fs-1" />
+                                            <FaJs className="icon" />
                                             <span className="icon-tooltip">Javascript</span>
                                         </div>
                                     </Col>
                                     <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                                         <div className="tech-icons">
-                                            <SiTypescript className="icon fs-1" />
+                                            <SiTypescript className="icon" />
                                             <span className="icon-tooltip">Typescript</span>
                                         </div>
                                     </Col>
@@ -88,13 +85,13 @@ function Stack() {
                                 <Row className="tech-row">
                                     <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                                         <div className="tech-icons">
-                                            <FaReact className="icon fs-1" />
+                                            <FaReact className="icon" />
                                             <span className="icon-tooltip">React</span>
                                         </div>
                                     </Col>
                                     <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                                         <div className="tech-icons">
-                                            <FaBootstrap className="icon fs-1" />
+                                            <FaBootstrap className="icon" />
                                             <span className="icon-tooltip">Bootstrap</span>
                                         </div>
                                     </Col>
@@ -102,13 +99,13 @@ function Stack() {
                                 <Row className="tech-row">
                                     <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                                         <div className="tech-icons">
-                                            <FaHtml5 className="icon fs-1" />
+                                            <FaHtml5 className="icon" />
                                             <span className="icon-tooltip">HTML</span>
                                         </div>
                                     </Col>
                                     <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                                         <div className="tech-icons">
-                                            <FaCss3Alt className="icon fs-1" />
+                                            <FaCss3Alt className="icon" />
                                             <span className="icon-tooltip">CSS</span>
                                         </div>
                                     </Col>
@@ -116,13 +113,13 @@ function Stack() {
                                 <Row className="tech-row">
                                     <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                                         <div className="tech-icons ">
-                                            <SiRedux className="icon fs-1" />
+                                            <SiRedux className="icon" />
                                             <span className="icon-tooltip">Redux</span>
                                         </div>
                                     </Col>
                                     <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                                         <div className="tech-icons">
-                                            <SiVite className="icon fs-1" />
+                                            <SiVite className="icon" />
                                             <span className="icon-tooltip">Vite</span>
                                         </div>
                                     </Col>
@@ -132,28 +129,28 @@ function Stack() {
                     </Card>
                 </Col>
 
-                <Col md={4} sm={6} className="my-stack-col">
+                <Col lg={4} md={6} sm={12} className="my-stack-col">
+                    <span className="tech-category-text">Other technologies</span>
                     <Card className="tech-card">
+
                         <Card.Img
                             variant="top"
                             src="/assets/notepad-img.png"
                             alt="Notepad"
                             className="notepad-img"
                         />
-
-
                         <Card className="overlaying-card position-absolute">
                             <div className="tech-card-img-container">
                                 <Row className="tech-row">
                                     <Col xs={6} sm={6} md={6} lg={6} xl={6} >
                                         <div className="tech-icons">
-                                            <FaGit className="icon  fs-1" />
+                                            <FaGit className="icon" />
                                             <span className="icon-tooltip">Git</span>
                                         </div>
                                     </Col>
                                     <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                                         <div className="tech-icons">
-                                            <SiPostman className="icon  fs-1" />
+                                            <SiPostman className="icon" />
                                             <span className="icon-tooltip">Postman</span>
                                         </div>
                                     </Col>
@@ -161,13 +158,13 @@ function Stack() {
                                 <Row className="tech-row">
                                     <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                                         <div className="tech-icons">
-                                            <IoLogoFirebase className="icon  fs-1" />
+                                            <IoLogoFirebase className="icon" />
                                             <span className="icon-tooltip">Firebase</span>
                                         </div>
                                     </Col>
                                     <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                                         <div className="tech-icons">
-                                            <DiVisualstudio className="icon  fs-1" />
+                                            <DiVisualstudio className="icon" />
                                             <span className="icon-tooltip">Visual Studio Code</span>
                                         </div>
                                     </Col>
@@ -175,13 +172,13 @@ function Stack() {
                                 <Row className="tech-row">
                                     <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                                         <div className="tech-icons">
-                                            <SiIntellijidea className="icon  fs-1" />
+                                            <SiIntellijidea className="icon" />
                                             <span className="icon-tooltip">Intellij</span>
                                         </div>
                                     </Col>
                                     <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                                         <div className="tech-icons">
-                                            <FaUnity className="icon fs-1" />
+                                            <FaUnity className="icon" />
                                             <span className="icon-tooltip">Unity</span>
                                         </div>
                                     </Col>
