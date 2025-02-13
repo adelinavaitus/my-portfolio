9e5fb0e80d42b40d2bfbaccedad0585c7668e844
Adelina Vaitus <adelinavaitus@gmail.com>
2025-02-13 19:05:04 +0200

    Added the hobbies section

--- my-portfolio/src/components/AboutMeComponent.jsx
+++ my-portfolio/src/components/AboutMeComponent.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import "../style/about-me.css"
+import { Row, Col, Card } from "react-bootstrap";
 
 function About() {
-    return <h1>About me page</h1>;
+    return (
+        <div>
+            <Row>
+                <Col>
+                    <Card>
+                        <Card.Header className="card-header-hobby">Knitting</Card.Header>
+                        <Card.Body>
+                            <Row>
+                                <Col>
+                                    <Card.Img src="/assets/hobbies/knitting.png" />
+                                </Col>
+                                <Col>
+                                    <Card.Text>
+                                        For me, knitting serves as a therapeutic escape, providing
+                                        peace and contentment. 
+                                    </Card.Text>
+                                </Col>
+                            </Row>
+                        </Card.Body>
+                    </Card>
+                </Col>
+
+                <Col>
+                    <Card>
+                        <Card.Header className="card-header-hobby">Traveling</Card.Header>
+                        <Card.Body>
+                            <Row>
+                                <Col><Card.Img src="/assets/hobbies/traveling.png" /></Col>
+                                <Col>
+                                <Card.Text>
+                                "Not all those who wander are lost." -
+                                J.R.R. Tolkien
+                                </Card.Text>
+                                </Col>
+                            </Row>
+                        </Card.Body>
+                    </Card>
+                </Col>
+
+                <Col>
+                    <Card>
+                        <Card.Header className="card-header-hobby">Reading</Card.Header>
+                        <Card.Body>
+                            <Row>
+                                <Col><Card.Img src="/assets/hobbies/reading.png" /></Col>
+                                <Col>
+                                    <Card.Text>
+                                    Books are the plane, and the train, and the road. 
+                                    They are the destination, and the journey. They are home.
+                                    </Card.Text>
+                                </Col>
+                            </Row>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </div>
+    );
 }
 
 export default About;
