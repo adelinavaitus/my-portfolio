1a9aee3b421a04cdea16afdae3b17f324e92410c
Adelina Vaitus <adelinavaitus@gmail.com>
2025-02-14 17:53:22 +0200

    Clean-up code and added comments for My stack component.

--- my-portfolio/src/components/MyStackComponent.jsx
+++ my-portfolio/src/components/MyStackComponent.jsx
@@ -5,9 +5,28 @@ import { FaArrowDown, FaGithub } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "../style/my-stack.css";
 
+// Array of notepad items to display the technologies
+const notepads = [
+    { title: "Backend", src: "/assets/notepad1.png" },
+    { title: "Frontend", src: "/assets/notepad2.png" },
+    { title: "Other technologies", src: "/assets/notepad3.png" },
+];
+
+// FlowerLine component for displaying a decorative flower line
+const FlowerLine = () => {
+    return (
+        <img
+            src="/assets/flower-line.png"
+            alt="Flower line image"
+            className="flower-line"
+        />
+    );
+};
+
 function Stack() {
     return (
         <div className="container">
+            {/* Introduction text */}
             <Row>
                 <p className="text-intro-stack">
                     As a software engineer, I have a versatile set of skills that allows
@@ -15,71 +34,42 @@ function Stack() {
                     Here's a peek into my tech toolkit.
                 </p>
             </Row>
+
+            {/* Loop over notepads array to display each tech category */}
             <Row className="row-container">
+                {notepads.map((notepad, index) => (
+                    <Col lg={4} md={6} sm={12} key={index} className="my-stack-col" >
 
-                <Col lg={4} md={6} sm={12} className="my-stack-col" >
-
-                    <Card className="tech-card ">
-                        <h5 className="title-tech-card">Backend</h5>
-                        <div className="empty-div"></div>
-                        <Card.Img
-                            variant="top"
-                            src="/assets/notepad1.png"
-                            alt="Notepad"
-                            className="notepad-img shadow"
-                        />
-                    </Card>
-                </Col>
-
-                <Col lg={4} md={6} sm={12} className="my-stack-col">
-                    <Card className="tech-card ">
-                        <h5 className="title-tech-card">Frontend</h5>
-                        <div className="empty-div"></div>
-                        <Card.Img
-                            variant="top"
-                            src="/assets/notepad2.png"
-                            alt="Notepad"
-                            className="notepad-img shadow"
-                        />
-                    </Card>
-                </Col>
-
-                <Col lg={4} md={6} sm={12} className="my-stack-col">
-                    <Card className="tech-card">
-                        <h5 className="title-tech-card">Other technologies</h5>
-                        <div className="empty-div"></div>
-                        <Card.Img
-                            variant="top"
-                            src="/assets/notepad3.png"
-                            alt="Notepad"
-                            className="notepad-img shadow"
-                        />
-                    </Card>
-                </Col>
-            </Row>
-            <Row>
-                <img
-                    src="/assets/flower-line.png"
-                    alt="Flower line image"
-                    className="flower-line"
-                />
+                        <Card className="tech-card ">
+                            <h5 className="title-tech-card">{notepad.title}</h5>
+                            <div className="empty-div"></div>
+                            <Card.Img
+                                variant="top"
+                                src={notepad.src}
+                                alt="Notepad"
+                                className={"notepad-img shadow"}
+                            />
+                        </Card>
+                    </Col>
+                ))}
             </Row>
 
+            {/* Decorative flower line */}
+            <Row><FlowerLine /></Row>
+
+            {/* Section with a link to the GitHub profile */}
             <Row className="github-row" >
                 <h4 className="h4-github">Browse through my work here:</h4>
                 <Link to="https://github.com/adelinavaitus">
                     <Button className="github-button shadow">Github <FaGithub />
                     </Button>
                 </Link>
+            </Row>
 
-            </Row>
-            <Row>
-                <img
-                    src="/assets/flower-line.png"
-                    alt="Flower line image"
-                    className="flower-line"
-                />
-            </Row>
+            {/* Decorative flower line */}
+            <Row><FlowerLine /></Row>
+
+            {/* Section for displaying certifications */}
             <Row className="row-container">
                 <div className="text-intro-stack">
                     <p>
