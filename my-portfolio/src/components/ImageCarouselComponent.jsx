c458270719e5afe3a2bd6ff0ba00df820728033c
Adelina Vaitus <adelinavaitus@gmail.com>
2025-02-14 17:42:51 +0200

    Clean-up code and added comments for Contact me and Carousel components.

--- my-portfolio/src/components/ImageCarouselComponent.jsx
+++ my-portfolio/src/components/ImageCarouselComponent.jsx
@@ -3,38 +3,40 @@ import { Carousel } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import "../style/my-stack.css";
 
+// Array of slides for the carousel with certificate links, image sources, and alt text
+const slides = [
+    {
+        to: "https://www.coursera.org/account/accomplishments/specialization/D9CRZ6196WZH",
+        src: "https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~D9CRZ6196WZH/CERTIFICATE_LANDING_PAGE~D9CRZ6196WZH.jpeg",
+        alt: "Java certificate"
+    },
+    {
+        to: "https://www.coursera.org/account/accomplishments/specialization/6XXLOAZPRI8U",
+        src: "https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~6XXLOAZPRI8U/CERTIFICATE_LANDING_PAGE~6XXLOAZPRI8U.jpeg",
+        alt: "Spring Framework certificate"
+    },
+    {
+        to: "https://www.coursera.org/account/accomplishments/verify/H35CPZ57F3V2",
+        src: "https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~H35CPZ57F3V2/CERTIFICATE_LANDING_PAGE~H35CPZ57F3V2.jpeg",
+        alt: "React certificate"
+    }
+];
+
 function ImageCarousel() {
     return (
         <Carousel className='carousel-container'>
-            <Carousel.Item>
-                <Link to="https://www.coursera.org/account/accomplishments/specialization/D9CRZ6196WZH">
-                    <img
-                        className="d-block w-100"
-                        src="https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~D9CRZ6196WZH/CERTIFICATE_LANDING_PAGE~D9CRZ6196WZH.jpeg"
-                        alt="Java certificate"
-                    />
-                </Link>
-            </Carousel.Item>
-
-            <Carousel.Item>
-                <Link to="https://www.coursera.org/account/accomplishments/specialization/6XXLOAZPRI8U">
-                    <img
-                        className="d-block w-100"
-                        src="https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~6XXLOAZPRI8U/CERTIFICATE_LANDING_PAGE~6XXLOAZPRI8U.jpeg"
-                        alt="Spring Framework certificate"
-                    />
-                </Link>
-            </Carousel.Item>
-
-            <Carousel.Item>
-                <Link to="https://www.coursera.org/account/accomplishments/verify/H35CPZ57F3V2">
-                    <img
-                        className="d-block w-100"
-                        src="https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~H35CPZ57F3V2/CERTIFICATE_LANDING_PAGE~H35CPZ57F3V2.jpeg"
-                        alt="React certificate"
-                    />
-                </Link>
-            </Carousel.Item>
+            {/* Iterating over each slide to render the carousel item */}
+            {slides.map((slide, index) => (
+                <Carousel.Item key={index}>
+                    <Link to={slide.to}>    {/* Link to the certificate page */}
+                        <img
+                            className="d-block w-100"   // Bootstrap class for full-width image
+                            src={slide.src}     // Image source for each slide
+                            alt={slide.alt}     // Alt text for each image for accessibility
+                        />
+                    </Link>
+                </Carousel.Item>
+            ))}
         </Carousel>
     );
 }
